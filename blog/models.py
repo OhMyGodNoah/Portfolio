bd03944645dfe7740afeda78849362e05759fc6b
Noah Greer <noahgreer.66@gmail.com>
2022-05-22 18:20:21 -0500

    made several changes

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
+import os
+
 from django.db import models
 
 # Create your models here.
+current_dir = os.getcwd()
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to=current_dir + '/media/images')
+    summary = models.CharField(max_length=200)
+    date = models.DateField()
